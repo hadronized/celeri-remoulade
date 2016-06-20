b8bd479e324eb9f48e8a1f5188c71c46b32db722
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-06-20 14:11:54 +0200

    Device.

--- lib/device.rs
+++ lib/device.rs
@@ -1,15 +1,15 @@
 use time;
 
-// The device is responsible of managing synchronization and audio playback by providing a playback
-// cursor to a closure used as demo.
+/// The device is responsible of managing synchronization and audio playback by providing a playback
+/// cursor to a closure used as demo.
 pub struct Device {
-  // Monotonic epoch. Set when a device is created. Nanoseconds.
-  //
-  // That epoch is used to re-synchronize audio playback if it starts lagging behind. It’s also of a
-  // good use when no audio playback is available – for instance when writting the first lines of
-  // a demo.
+  /// Monotonic epoch. Set when a device is created. Nanoseconds.
+  ///
+  /// That epoch is used to re-synchronize audio playback if it starts lagging behind. It’s also of a
+  /// good use when no audio playback is available – for instance when writting the first lines of
+  /// a demo.
   epoch: u64,
-  // Cached playback cursor.
+  /// Cached playback cursor.
   cursor: f32,
 }
 
@@ -23,12 +23,12 @@ impl Device {
     }
   }
 
-  // Recompute the playback cursor.
+  /// Recompute the playback cursor.
   pub fn recompute_playback_cursor(&mut self) {
     self.cursor = (time::precise_time_ns() - self.epoch) as f32 * NANOSECOND_TH
   }
 
-  // Playback cursor in seconds.
+  /// Playback cursor in seconds.
   pub fn playback_cursor(&self) -> f32 {
     self.cursor
   }
