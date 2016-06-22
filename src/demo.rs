cac2df02a55a1659e786c9d1b9019168e1f5e77b
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-06-22 14:31:22 +0200

    Chromatic aberration enabled again.

--- src/demo.rs
+++ src/demo.rs
@@ -120,7 +120,7 @@ pub fn init(w: u32, h: u32, kbd: Keyboard, mouse: Mouse, mouse_mv: MouseMove, sc
       jitter.update(line_jitter);
     });
 
-    Pipeline::new(&back_buffer, [0., 0., 0., 1.], vec![
+    Pipeline::new(&chromatic_aberration_buffer, [0., 0., 0., 1.], vec![
       &ShadingCommand::new(&chess_program, |_|{}, vec![
         RenderCommand::new(None,
                            true,
@@ -146,21 +146,21 @@ pub fn init(w: u32, h: u32, kbd: Keyboard, mouse: Mouse, mouse_mv: MouseMove, sc
     ]).run();
 
     // apply the chromatic shader and output directly into the back buffer
-    //Pipeline::new(&back_buffer, [0., 0., 0., 1.], vec![
-    //  &ShadingCommand::new(&chromatic_aberration_program,
-    //                       |&(ref tex, ref ires)| {
-    //                         tex.update(&chromatic_aberration_buffer.color_slot.texture);
-    //                         ires.update([1. / w as f32, 1. / h as f32]);
-    //                       },
-    //                       vec![
-    //                         RenderCommand::new(None,
-    //                                            true,
-    //                                            |_|{},
-    //                                            &plane.object,
-    //                                            1,
-    //                                            None)
-    //                       ])
-    //]).run();
+    Pipeline::new(&back_buffer, [0., 0., 0., 1.], vec![
+      &ShadingCommand::new(&chromatic_aberration_program,
+                           |&(ref tex, ref ires)| {
+                             tex.update(&chromatic_aberration_buffer.color_slot.texture);
+                             ires.update([1. / w as f32, 1. / h as f32]);
+                           },
+                           vec![
+                             RenderCommand::new(None,
+                                                true,
+                                                |_|{},
+                                                &plane.object,
+                                                1,
+                                                None)
+                           ])
+    ]).run();
 
     true
   }))
