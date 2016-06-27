1ca9e1e60b3bb71e25e905e803da3996c48f5b23
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-06-28 00:25:48 +0200

    What should I do with that?

--- src/shaders/bloom.rs
+++ src/shaders/bloom.rs
@@ -1,6 +1,9 @@
 use ion::shader::{Program, ProgramError, new_program};
-use luminance::{Dim2, Flat, RGBA32F};
-use luminance_gl::gl33::{Texture, Uniform};
+use ion::objects::new_plane;
+use luminance::{Equation, Factor, Dim2, Flat, RGBA32F};
+use luminance::pipeline::{Pipeline, SomeShadingCommand};
+use luminance_gl::gl33::{Framebuffer, RenderCommand, ShadingCommand, Tessellation, Texture, Slot,
+                         Uniform};
 use std::fmt::Write;
 
 const BLOOM_VS: &'static str = "\
@@ -68,4 +71,55 @@ void main() {\n\
 ") + &gen_str_kernel(kernel, horiz) + "\n\
   frag = vec4(color, 1.);\n\
 }"
+}
+
+pub struct BlurTechnique<'a> {
+  // horizontal blur
+  hblur_buffer: Framebuffer<Flat, Dim2, Slot<Flat, Dim2, RGBA32F>, ()>,
+  hblur_program: BloomProgram<'a>,
+  // vertical blur
+  vblur_buffer: Framebuffer<Flat, Dim2, Slot<Flat, Dim2, RGBA32F>, ()>,
+  vblur_program: BloomProgram<'a>,
+  // plane used to perform fullscreen passes
+  plane: Tessellation,
+  w: u32,
+  h: u32
+}
+
+impl<'a> BlurTechnique<'a> {
+  pub fn new(w: u32, h: u32, kernel: &[f32]) -> Self {
+    BlurTechnique {
+      hblur_buffer: Framebuffer::new((w, h), 0).unwrap(),
+      hblur_program: new_bloom_program(kernel, true).unwrap(),
+      vblur_buffer: Framebuffer::new((w, h), 0).unwrap(),
+      vblur_program: new_bloom_program(kernel, false).unwrap(),
+      plane: new_plane(),
+      w: w,
+      h: h
+    }
+  }
+
+  pub fn apply(&self, shading_commands: Vec<&SomeShadingCommand>) {
+    // run the shading commands into the horizontal blur buffer
+    Pipeline::new(&self.hblur_buffer, [0., 0., 0., 1.], shading_commands).run();
+
+    // apply the horizontal blur and output the result into the vertical blur buffer
+    Pipeline::new(&self.vblur_buffer, [0., 0., 0., 1.], vec![
+      &ShadingCommand::new(&self.hblur_program,
+                           |&(ref tex, ref ires)| {
+                             tex.update(&self.hblur_buffer.color_slot.texture);
+                             ires.update([1. / self.w as f32, 1. / self.h as f32]);
+                           },
+                           vec![
+                             RenderCommand::new(Some((Equation::Additive, Factor::One, Factor::One)),
+                                                true,
+                                                |_|{},
+                                                &self.plane,
+                                                1,
+                                                None)
+                           ])
+    ]).run();
+
+    // apply the blur and append the result to the output
+  }
 }
