2e4e302e84eee514f4b018f04a832a19b8319f31
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-07-01 09:09:02 +0200

    Ashes.

---
+++ src/parts/ashes.rs
@@ -0,0 +1,12 @@
+use luminance::Mode;
+use luminance_gl::gl33::Tessellation;
+use procedural::noise2;
+
+pub fn new_ashes(nb: usize) -> Tessellation {
+  let points: Vec<_> = (0..nb).map(|i| {
+    let i =  i as f32;
+    [noise2(i, i), noise2(-i, nb as f32)]
+  }).collect();
+
+  Tessellation::new(Mode::Point, &points, None)
+}
--- src/parts/mod.rs
+++ src/parts/mod.rs
@@ -1,1 +1,2 @@
+pub mod ashes;
 pub mod lines;
