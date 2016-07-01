b0cc101a25c5bc21f835bb5954c3beb230d2af7b
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-07-01 13:49:52 +0200

    Randomization of ashes.

--- src/parts/ashes.rs
+++ src/parts/ashes.rs
@@ -5,7 +5,7 @@ use procedural::noise2;
 pub fn new_ashes(nb: usize) -> Tessellation {
   let points: Vec<_> = (0..nb).map(|i| {
     let i =  i as f32;
-    [noise2(i, i), noise2(-i, nb as f32)]
+    [noise2(i, i.powf(2.)), noise2(-i, i * nb as f32)]
   }).collect();
 
   Tessellation::new(Mode::Point, &points, None)
