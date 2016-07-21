2f8cb0b4f9acd9bc1824a8f55a57b925076a55a0
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-07-21 20:15:58 +0200

    Vignette.

--- src/shaders/lines_pp.rs
+++ src/shaders/lines_pp.rs
@@ -29,6 +29,9 @@ uniform sampler2D tex;\n\
 uniform vec2 ires;\n\
 \n\
 void main() {\n\
+  vec4 color = vec4(0., 0., 0., 1.);\n\
+
+  // chromatic aberration\n\
   vec2 off = ires * pow(abs(length(v_screen_co)), 2.) * 3.;\n\
   float r0 = texture(tex, v_co - vec2(off.x, off.y)).r;\n\
   float r1 = texture(tex, v_co).r;\n\
@@ -39,7 +42,13 @@ void main() {\n\
   float b0 = texture(tex, v_co + vec2(off.x, -off.y)).b;\n\
   float b1 = texture(tex, v_co - vec2(off.x, 0.)).b;\n\
   float b2 = texture(tex, v_co + vec2(0., off.y)).b;\n\
-  frag = vec4((r0 + r1 + r2) / 3., (g0 + g1 + g2) / 3., (b0 + b1 + b2) / 3., 1.);\n\
+  color = vec4((r0 + r1 + r2) / 3., (g0 + g1 + g2) / 3., (b0 + b1 + b2) / 3., 1.);\n\
+
+  // vignette\n\
+  color *= mix(1., .5, pow(length(v_screen_co), 2.));\n\
+
+  // output
+  frag = color;\n\
 }";
 
 pub type LinesPP<'a> = Program<(Uniform<&'a Texture<Flat, Dim2, RGBA32F>>, Uniform<[f32; 2]>)>;
